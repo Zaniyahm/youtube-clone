c85687492623abb5268be0cb4e997564591bff3b
Zaniyahm <145847562+Zaniyahm@users.noreply.github.com>
2023-12-24 18:39:04 +0530

    Create  Videos.jsx

---
+++ src/components/Videos.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard, Loader } from "./";
+
+const Videos = ({ videos, justifyContent, direction }) => {
+  if (!videos?.length) return <Loader />;
+
+  return (
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent={{ xs: "center", md: justifyContent }}
+      gap={{ md: 3, xs: 2 }}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
